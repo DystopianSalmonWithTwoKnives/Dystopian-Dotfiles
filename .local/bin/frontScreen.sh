68ce2722f60b3d72f8e5530476802cb8ce5a17bf
DystopianSalmonWithTwoKnives <DystopianSalmonWithTwoKnives>
2019-12-17 21:59:02 -0500

    Made the arrangement more beautifull still need to change the numbers from hardcoded to variables

--- .local/bin/frontScreen.sh
+++ .local/bin/frontScreen.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
     st -c front & 
-    xdotool windowactivate $(xdotool search --class front)
+    # xdotool windowactivate $(xdotool search --class front)
     sleep 0.1
     xdotool type "export PS1=\"\""
     xdotool key Return
     sleep 0.1
-    xdotool type "echo -e \"\n\n\" && neofetch | lolcat -s 201 && printf '\\33[?25l'"
+    xdotool type "echo -e \"\n\n\n\n\" && neofetch | lolcat -s 201 && printf '\\33[?25l'"
     xdotool key Return
     xdotool key Shift_L+Alt_L+space
     sleep 0.1
     xdotool keydown Alt_L
     xdotool mousedown 3
     sleep 0.1
-    xdotool mousemove 630 400
+    xdotool mousemove 630 420
     sleep 0.1
     xdotool mouseup 3
     xdotool mousedown 1
     sleep 0.1
-    xdotool mousemove_relative 50 330
+    xdotool mousemove_relative 50 323
     sleep 0.1
     xdotool mouseup 1
     xdotool keyup Alt_L
 
     st -c front1 & 
-    xdotool windowactivate $(xdotool search --class front1)
+    # xdotool windowactivate $(xdotool search --class front1)
     sleep 0.1
     xdotool type "peaclock"
     xdotool key Return
@@ -46,19 +46,19 @@
     xdotool keyup Alt_L
 
     st -c front2 & 
-    xdotool windowactivate $(xdotool search --class front2)
+    # xdotool windowactivate $(xdotool search --class front2)
     sleep 0.1
     xdotool key Shift_L+Alt_L+space
     sleep 0.1
     xdotool keydown Alt_L
     xdotool mousedown 3
     sleep 0.1
-    xdotool mousemove 630 670
+    xdotool mousemove 630 680
     sleep 0.1
     xdotool mouseup 3
     xdotool mousedown 1
     sleep 0.1
-    xdotool mousemove_relative 700 40
+    xdotool mousemove_relative 700 50
     sleep 0.1
     xdotool mouseup 1
     xdotool keyup Alt_L
