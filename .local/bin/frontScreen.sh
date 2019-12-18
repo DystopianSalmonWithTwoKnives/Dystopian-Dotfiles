2dd5902b7a342feda92c4cb39edfd95a6bb8b467
DystopianSalmonWithTwoKnives <DystopianSalmonWithTwoKnives>
2019-12-17 23:29:31 -0500

    Deleting some sleep to augment execution speed

--- .local/bin/frontScreen.sh
+++ .local/bin/frontScreen.sh
@@ -2,64 +2,51 @@
 
     st -c front & 
     # xdotool windowactivate $(xdotool search --class front)
-    sleep 0.1
     xdotool type "export PS1=\"\""
     xdotool key Return
-    sleep 0.1
     xdotool type "echo -e \"\n\n\n\n\" && neofetch | lolcat -s 201 && printf '\\33[?25l'"
     xdotool key Return
     xdotool key Shift_L+Alt_L+space
-    sleep 0.1
     xdotool keydown Alt_L
     xdotool mousedown 3
-    sleep 0.1
+    sleep 0.05
     xdotool mousemove 630 420
-    sleep 0.1
     xdotool mouseup 3
     xdotool mousedown 1
-    sleep 0.1
     xdotool mousemove_relative 50 323
-    sleep 0.1
     xdotool mouseup 1
     xdotool keyup Alt_L
 
     st -c front1 & 
     # xdotool windowactivate $(xdotool search --class front1)
-    sleep 0.1
+    sleep 0.05
     xdotool type "peaclock"
     xdotool key Return
     xdotool key Shift_L+Alt_L+space
     xdotool key s
-    sleep 0.1
+    sleep 0.05
     xdotool key Escape
     xdotool keydown Alt_L
     xdotool mousedown 3
-    sleep 0.1
+    sleep 0.05
     xdotool mousemove 630 270
-    sleep 0.1
     xdotool mouseup 3
     xdotool mousedown 1
-    sleep 0.1
     xdotool mousemove_relative 50 50
-    sleep 0.1
     xdotool mouseup 1
     xdotool keyup Alt_L
 
     st -c front2 & 
     # xdotool windowactivate $(xdotool search --class front2)
-    sleep 0.1
+    sleep 0.05
     xdotool key Shift_L+Alt_L+space
-    sleep 0.1
     xdotool keydown Alt_L
     xdotool mousedown 3
-    sleep 0.1
+    sleep 0.05
     xdotool mousemove 630 680
-    sleep 0.1
     xdotool mouseup 3
     xdotool mousedown 1
-    sleep 0.1
     xdotool mousemove_relative 700 50
-    sleep 0.1
     xdotool mouseup 1
     xdotool keyup Alt_L
 
